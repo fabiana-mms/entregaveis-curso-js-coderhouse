d11200130a05cac02c6698e74d4b0c2d58d8b4c3
Fabiana Monteiro Menocci da Silva <fabi_mms_05@hotmail.com>
2023-05-29 21:12:56 -0300

    Commit da entrega da primeira atividade do curso de JavaScript

---
+++ exercicio01.js
@@ -0,0 +1,7 @@
+let seuNome = prompt("Insira seu nome");
+let seuEmail = prompt("Insira seu E-mail");
+
+let seuAcesso = "Para seu acesso, utilize: " + "seu nome " + seuNome + " e o seu e-mail " + seuEmail;
+
+alert(seuAcesso);
+console.log(seuAcesso);
