e7111c254c82b9fb4b3293760909e13d84282f9c
Fabiana Monteiro Menocci da Silva <fabi_mms_05@hotmail.com>
2023-06-04 14:51:18 -0300

    Entrega da atividade 2 dos exercicios de JavaScript

--- exercicio01.js
+++
@@ -1,7 +0,0 @@
-let seuNome = prompt("Insira seu nome");
-let seuEmail = prompt("Insira seu E-mail");
-
-let seuAcesso = "Para seu acesso, utilize: " + "seu nome " + seuNome + " e o seu e-mail " + seuEmail;
-
-alert(seuAcesso);
-console.log(seuAcesso);
